2c9c4bb15c70b3b7639688c7f51bf4dfc07f9f3c
BridneyPruett <59849987+BridneyPruett@users.noreply.github.com>
2020-04-27 20:10:58 -0400

    Update Worker.java

--- src/Worker.java
+++ src/Worker.java
@@ -1,2 +1,30 @@
-public class Worker {
+public class Worker extends Person {
+
+
+    private double hourlyPayRate;
+    private getHourlyPayRate();
+    {
+        retun this.hourPayRate;
+    }
+    public void setHourlyPayRate(double hourlypayrate)
+    {
+        this.hourPayRate=hourlypayrate;
+    }
+    public double calculateWeeklyPay(double hoursWorked)
+    {
+        if (hourlyWorked<=40)
+        {
+            return hoursWorked*this.hourlyPayRate;
+        }
+        else {
+            double regularpay=(hoursWorked-40)*(this.hourslyPayRate*1.5);
+        }
+    }
+
+    regularhours = 40.0;
 }
+    String regularpaystring = "Hours worked Pay Rate: " + regularhours + " and she will recieve: " + regularpay;
+    String overtimepaystring = "Hours worked at overtime Pay Rate: " + (hoursWorked - regularhours) + " and she will recieve: " + overtimepay;
+return regularpaystring + "\n" + overtimepaystring;
+        }
+        }
