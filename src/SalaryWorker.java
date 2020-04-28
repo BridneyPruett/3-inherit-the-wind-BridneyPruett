a1c5d4fed299e76c6ba9155dae308a5dac054477
BridneyPruett <59849987+BridneyPruett@users.noreply.github.com>
2020-04-27 20:09:09 -0400

    Update SalaryWorker.java

--- src/SalaryWorker.java
+++ src/SalaryWorker.java
@@ -1,4 +1,26 @@
-public class SalaryWorker extends Worker {
+public class SalaryWorker extends worker
 
-    public double annualSalary;
-}
+    {
+   
+      private double annualSalary;
+        public SalaryWorker(String ID, String fname, String lname, String title, int YOB, double hourlypayrate, double annualsalary)
+        {
+            super(ID, fname, lname, title, YOB, hourlypayrate);
+            this.setAnnualSalary(annualsalary);
+        }
+        public double getAnnualSalary()
+        {
+            return this.annualSalary;
+        }
+        public void setAnnualSalary(double annualsalary)
+        {
+            this.annualSalary = annualsalary;
+        }
+        public double calculateWeeklyPay(double hoursWorked)
+        {
+            return this.annualSalary / 52;
+        }
+        public String displayWeeklyPay(double hoursWorked)
+        {
+            return "Weekly pay: " + calculateWeeklyPay(0.0);
+        }
